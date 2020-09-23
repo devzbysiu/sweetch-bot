d7146b74693054bede96da2419d39a4f6e376ce0
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2020-09-23 18:30:16 +0200

    Add scheduler

--- src/cron.rs
+++ src/cron.rs
@@ -1,6 +1,25 @@
 use anyhow::Result;
+use clokwerk::{Scheduler, TimeUnits};
+use log::debug;
+use std::thread;
+use std::time::Duration;
 
-pub(crate) fn schedule<F: FnOnce() -> Result<()>>(fun: F) -> Result<()> {
-    fun()?;
-    Ok(())
+pub(crate) fn schedule<F>(mut fun: F) -> !
+where
+    F: 'static + FnMut() -> Result<()> + Send,
+{
+    let mut scheduler = Scheduler::new();
+    scheduler
+        .every(1.day())
+        .at("6:25 pm")
+        .run(move || fun().expect("failed to run fun in scheduler"));
+    run(&mut scheduler);
+}
+
+fn run(scheduler: &mut Scheduler) -> ! {
+    loop {
+        scheduler.run_pending();
+        debug!("sleeping");
+        thread::sleep(Duration::from_secs(10));
+    }
 }
