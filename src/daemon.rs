c153848c809ab26b542e1360effc32b45aa8a599
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2020-09-26 14:42:21 +0200

    Move log files to system tmp dir

--- src/daemon.rs
+++ src/daemon.rs
@@ -1,15 +1,20 @@
 use anyhow::Result;
 use daemonize::Daemonize;
 use std::fs::File;
+use std::path::{Path, PathBuf};
 
 pub(crate) fn daemonize<F: FnOnce() -> Result<()>>(fun: F) -> Result<()> {
     let daemonize = Daemonize::new()
-        .stdout(File::create("/tmp/sweetch-bot.out")?)
-        .stderr(File::create("/tmp/sweetch-bot.log")?);
+        .stdout(File::create(log_path("sweetch-bot.out"))?)
+        .stderr(File::create(log_path("sweetch-bot.log"))?);
 
     match daemonize.start() {
         Ok(_) => fun()?,
         Err(e) => eprintln!("Error, {}", e),
     }
     Ok(())
+}
+
+fn log_path(filename: &str) -> PathBuf {
+    Path::new(&std::env::temp_dir()).join(filename)
 }
