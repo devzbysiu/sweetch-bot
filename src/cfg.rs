9c51e93e10eb15ebf6c250a6dd2c72cdc87ad5b9
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2020-10-31 10:46:52 +0100

    tests: Cover load debug config with tests

--- src/cfg.rs
+++ src/cfg.rs
@@ -74,6 +74,7 @@ pub(crate) struct WatchedGame {
 }
 
 impl WatchedGame {
+    #[cfg(test)]
     pub(crate) fn new<S: Into<String>>(title: S) -> Self {
         Self {
             title: title.into(),
@@ -81,6 +82,7 @@ impl WatchedGame {
         }
     }
 
+    #[cfg(test)]
     pub(crate) fn with_acceptable_price(mut self, price: f64) -> Self {
         self.acceptable_price = Some(price);
         self
@@ -92,5 +94,105 @@ impl WatchedGame {
 
     pub(crate) fn acceptable_price(&self) -> Option<f64> {
         self.acceptable_price
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_load_debug_config_without_debug_option() {
+        // given
+        let config_content = r#"
+            [[watched_game]]
+            title = "Game 2 title here"
+        "#;
+
+        // when
+        let dbg_config = Config::load::<DebugConfig>(config_content).unwrap();
+
+        // then
+        assert_eq!(dbg_config.debug_enabled(), false);
+    }
+
+    #[test]
+    fn test_load_debug_config_with_debug_option_set_to_false() {
+        // given
+        let config_content = "debug = false";
+
+        // when
+        let dbg_config = Config::load::<DebugConfig>(config_content).unwrap();
+
+        // then
+        assert_eq!(dbg_config.debug_enabled(), false);
+    }
+
+    #[test]
+    fn test_load_debug_config_with_debug_enabled() {
+        // given
+        let config_content = "debug = true";
+
+        // when
+        let dbg_config = Config::load::<DebugConfig>(config_content).unwrap();
+
+        // then
+        assert_eq!(dbg_config.debug_enabled(), true);
+    }
+
+    #[test]
+    fn test_load_debug_config_with_real_life_config_and_debug_enabled() {
+        // given
+        let config_content = r#"
+            debug = true
+
+            [schedule]
+            run_at = ["7:00 pm"]
+
+            [[watched_game]]
+            title = "Game 1 title here"
+
+            [[watched_game]]
+            title = "Game 2 title here"
+        "#;
+
+        // when
+        let dbg_config = Config::load::<DebugConfig>(config_content).unwrap();
+
+        // then
+        assert_eq!(dbg_config.debug_enabled(), true);
+    }
+
+    #[test]
+    fn test_load_debug_config_with_real_life_config_and_without_debug() {
+        // given
+        let config_content = r#"
+            [schedule]
+            run_at = ["7:00 pm"]
+
+            [[watched_game]]
+            title = "Game 1 title here"
+
+            [[watched_game]]
+            title = "Game 2 title here"
+        "#;
+
+        // when
+        let dbg_config = Config::load::<DebugConfig>(config_content).unwrap();
+
+        // then
+        assert_eq!(dbg_config.debug_enabled(), false);
+    }
+
+    #[test]
+    fn test_load_debug_config_with_empty_config() {
+        // given
+        let config_content = "";
+
+        // when
+        let dbg_config = Config::load::<DebugConfig>(config_content).unwrap();
+
+        // then
+        assert_eq!(dbg_config.debug_enabled(), false);
     }
 }
